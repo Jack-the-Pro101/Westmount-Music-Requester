0ace3af41dbcaa9951c0c5fff35da9bfc9c22d9f
infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-11-06 15:43:30 -0500

    feat: full stack

---
+++ client/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [preact()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                changeOrigin: true,
+            },
+        },
+    },
+});
