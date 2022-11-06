befe7461d41a184f2f144156c27b0f73d8bad31f
infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-11-06 15:31:09 -0500

    feat: app

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__credits">Built by Emperor of Bluegaria (Jack H.)</p>
-    </footer>
-  );
-}
