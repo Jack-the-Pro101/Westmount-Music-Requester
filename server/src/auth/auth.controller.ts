48e44de1187827cb514404e29cd7e90a660e58e3
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-11-10 19:16:01 -0500

    fix: auth

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { GoogleOAuthGuard } from "./google-oauth.guard";
 import { Controller, Get, Request, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller("/auth")
+@Controller("/api/auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
