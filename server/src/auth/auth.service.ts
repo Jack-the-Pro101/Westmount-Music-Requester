0808ac4d6a5838d441a19bbf024b4184f18a0845
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-12-08 21:20:19 +0000

    fix: authentication

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -15,11 +15,11 @@ export class AuthService {
     return req.user;
   }
 
-  async validateUser(username: string, password: string): Promise<StoredUser | null | false> {
+  async validateUser(username: string, password: string): Promise<StoredUser | undefined> {
     const user = await this.usersService.findOne(username, true);
 
-    if (!user) return null;
-    if (!(await bcrypt.compare(password, user.password))) return false;
-    return user;
+    if (!user || user.type !== "INTERNAL") return;
+    if (!(await bcrypt.compare(password, user.password))) return;
+    return user as StoredUser;
   }
 }
