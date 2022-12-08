6dc00ac86d875e456601b84407644599f5519aac
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-12-08 21:05:01 +0000

    fix: user typing

--- server/src/auth/users.service.ts
+++ server/src/auth/users.service.ts
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 
 import Users from "src/models/User";
 
+type User = typeof Users extends mongoose.Model<infer T> ? T : unknown;
+
 @Injectable()
 export class UsersService {
-  async findOne(usernameOrEmail: string, internal = false): Promise<any> {
-    // TODO: convert to Users mongoose schema
+  async findOne(usernameOrEmail: string, internal = false): Promise<User> {
     try {
       return await Users.findOne(internal ? { username: usernameOrEmail } : { email: usernameOrEmail });
     } catch (err) {
@@ -14,7 +15,7 @@ export class UsersService {
     }
   }
 
-  async getOrCreateOne(usernameOrEmail: string, internal = false, data: any): Promise<any> {
+  async getOrCreateOne(usernameOrEmail: string, internal = false, data: Partial<User>): Promise<User> {
     try {
       const user = await Users.findOne(internal ? { username: usernameOrEmail } : { email: usernameOrEmail });
       if (user == null) return await Users.create(data);
