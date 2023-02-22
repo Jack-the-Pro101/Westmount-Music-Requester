088129d4b84589dda9c84b65cfad977a1920fa0b
Jack <canadajackhuang@gmail.com>
2023-02-22 15:29:11 -0500

    Update production callbacks

--- server/src/auth/google.strategy.ts
+++ server/src/auth/google.strategy.ts
@@ -23,7 +23,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/auth/google-redirect",
+      callbackURL: process.env.NODE_ENV === "production" ? process.env.GOOGLE_CALLBACK : "http://localhost:3000/api/auth/google-redirect",
       scope: ["email", "profile"],
     });
   }
