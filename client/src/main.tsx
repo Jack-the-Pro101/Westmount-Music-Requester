94352a9c1114282de9c18382a6e5f6b481d1a65f
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2023-05-18 15:07:57 +0000

    fix: js in html file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,13 @@
 import { render } from "preact";
 import { App } from "./app";
 
+const theme = localStorage.getItem("theme");
+const root = document.querySelector("html") as HTMLHtmlElement;
+
+if (theme === "light") {
+  root.classList.add("light");
+} else {
+  root.classList.remove("light");
+}
+
 render(<App />, document.getElementById("app") as HTMLElement);
